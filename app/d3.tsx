116efc9accede51a507825d9c1be612bb424e2f1
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2024-02-23 08:41:43 -0500

    build d3 wip

---
+++ app/d3.tsx
@@ -0,0 +1,4 @@
+import * as d3 from "d3";
+
+// Could not find a declaration file for module 'd3'. '/Users/evan/pt-react/node_modules/d3/src/index.js' implicitly has an 'any' type.
+//   Try `npm i --save-dev @types/d3` if it exists or add a new declaration (.d.ts) file containing `declare module 'd3';`
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,30 @@
+import StateData from "./d3Geo/states-albers-10m.json";
 import Image from "next/image";
 import styles from "@/app/ui/home.module.css";
 import { Roboto_Mono } from "next/font/google";
 const robotoMono = Roboto_Mono({ subsets: ["latin"] });
+import * as d3 from "d3";
+
+const map = () => {
+  return <div id="map">map.</div>;
+};
 
 export default function Home() {
+  const renderMap = () => {
+    const path = d3.geoPath();
+    const svg = d3
+      .select("#map")
+      .append("svg")
+      .attr("width", 1000)
+      .attr("height", 600);
+    const g = svg.append("g");
+    const states = topojson.feature(StateData, Statedata.objects.states);
+  };
   return (
     <main
       className={`${robotoMono.className}flex min-h-screen flex-col items-center justify-between p-24`}
     >
+      {Home()}
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         {/* <div className={styles.shape} /> */}
         <p className="text-3xl  fixed left-0 top-0 flex w-full justify-center border-b border-gray-300  pb-6 pt-8 lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -15,21 +32,14 @@ export default function Home() {
           <code className="roboto">Population Tracker</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-25 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-5 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer  "
-          >
-            <Image
+          {/* <Image
               src="/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
               width={80}
               height={50}
               priority
-            />
-          </a>
+            /> */}
         </div>
       </div>
     </main>
