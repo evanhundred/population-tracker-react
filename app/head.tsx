55a31b47d1ef6e096548f1cfcff1c94e14a5ddee
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2024-02-23 03:24:17 -0500

    ingegrate google fonts

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+const HeadComponent = () => {
+  return (
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+    </Head>
+  );
+};
